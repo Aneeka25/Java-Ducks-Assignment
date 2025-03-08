5646213d2d2ce99b00ca93aa0a1c7d4d707d72c4
Aneeka25 <154527297+Aneeka25@users.noreply.github.com>
2025-03-08 16:51:20 +0530

    Update RedheadDuck.java with display methods

--- RedheadDuck.java
+++ RedheadDuck.java
@@ -5,3 +5,5 @@ class RedheadDuck extends Duck {
         quackBehavior = new Quack();
         swimBehavior = new Swim();
     }
+    void display() { System.out.println("I am a Redhead Duck"); }
+}
