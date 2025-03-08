a0e8fd95a054fd14c39ff9f485fde1cf419fed17
Aneeka25 <154527297+Aneeka25@users.noreply.github.com>
2025-03-08 16:52:18 +0530

    Update MallardDuck.java with display methods

--- MallardDuck.java
+++ MallardDuck.java
@@ -5,3 +5,5 @@ class MallardDuck extends Duck {
         quackBehavior = new Quack();
         swimBehavior = new Swim();
     }
+    void display() { System.out.println("I am a Mallard Duck"); }
+}
