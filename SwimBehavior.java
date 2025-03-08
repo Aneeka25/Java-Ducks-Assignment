9c29e307578cde9c3a6b74eb4da9c8d92302c605
Aneeka25 <154527297+Aneeka25@users.noreply.github.com>
2025-03-08 17:00:41 +0530

    Implement various swimming behaviors

--- SwimBehavior.java
+++ SwimBehavior.java
@@ -2,3 +2,14 @@
 interface SwimBehavior {
     void swim();
 }
+class Swim implements SwimBehavior {
+    public void swim() { System.out.println("I am swimming!"); }
+}
+
+class Float implements SwimBehavior {
+    public void swim() { System.out.println("I am floating!"); }
+}
+
+class Drown implements SwimBehavior {
+    public void swim() { System.out.println("I am drowning!"); }
+}
