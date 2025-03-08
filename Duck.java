6720fda645dedde7e8f96317815bc680ad1bc47b
Aneeka25 <154527297+Aneeka25@users.noreply.github.com>
2025-03-08 16:55:16 +0530

    Implement behavior execution methods

--- Duck.java
+++ Duck.java
@@ -3,3 +3,10 @@ abstract class Duck {
     FlyBehavior flyBehavior;
     QuackBehavior quackBehavior;
     SwimBehavior swimBehavior;
+
+    void performFly() { flyBehavior.fly(); }
+    void performQuack() { quackBehavior.quack(); }
+    void performSwim() { swimBehavior.swim(); }
+
+
+    
