72312aa86dd5a9e4d03f9a32573f887000ed98af
Aneeka25 <154527297+Aneeka25@users.noreply.github.com>
2025-03-08 17:04:05 +0530

    Implement various flying behaviors

--- FlyBehavior.java
+++ FlyBehavior.java
@@ -2,3 +2,10 @@
 interface FlyBehavior {
     void fly();
 }
+class Fly implements FlyBehavior {
+    public void fly() { System.out.println("I am flying!"); }
+}
+
+class NotFly implements FlyBehavior {
+    public void fly() { System.out.println("I cannot fly."); }
+}
